b6cf51958b299d8a115b11d9d5a59239f8e6e0a4
eribertt_fedorabox <erimendz@gmail.com>
2021-08-12 12:17:41 +0300

    add new variant of assert macro

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ fn get_process_id() -> u32 {
 }
 
 // add unit test codes
+// note the assert macro is used to check whether the condition evaluates to true
+// there are 2 other assert macros e.g. assert_eq! and assert_ne!
 #[test]
 fn test_if_process_id_is_returned() {
-	assert!(get_process_id() > 0);
+	// assert!(get_process_id() > 0);
+	// another way to use assert macro
+	// this time with custom error message
+	assert_ne!(get_process_id(), 0, "There is error in code");
 }
